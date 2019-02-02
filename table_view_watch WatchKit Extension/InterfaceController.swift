015676e1e641930e0545d9f5e41e5195673f3aad
shane_WL <ho.wai.loon@accenture.com>
2019-02-02 23:06:02 +0800

    Initial Commit

---
+++ table_view_watch WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  table_view_watch WatchKit Extension
+//
+//  Created by ho.wai.loon on 02/02/2019.
+//  Copyright © 2019 Shane. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
