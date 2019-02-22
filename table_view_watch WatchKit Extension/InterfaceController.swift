9b0fe50d01ed7f6c6fe9460ba37185e90f289bfd
shane_WL <ho.wai.loon@accenture.com>
2019-02-22 10:50:53 +0800

    Initial Commit

--- table_view_watch WatchKit Extension/InterfaceController.swift
+++ table_view_watch WatchKit Extension/InterfaceController.swift
@@ -12,9 +12,12 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var tableView: WKInterfaceTable!
+    
+    let tableData = ["One","Two","Three","Four","Five","Six"]
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
+        loadTableData()
         // Configure interface objects here.
     }
     
@@ -28,4 +31,12 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    private func loadTableData() {
+        tableView.setNumberOfRows(tableData.count, withRowType: "RowController")
+        for (index, rowModel) in tableData.enumerated(){
+            if let rowController = tableView.rowController(at: index) as? RowController {
+                rowController.rowLabel.setText(rowModel)
+            }
+        }
+    }
 }
---
+++ table_view_watch WatchKit Extension/RowController.swift
@@ -0,0 +1,15 @@
+//
+//  RowController.swift
+//  table_view_watch WatchKit Extension
+//
+//  Created by ho.wai.loon on 02/02/2019.
+//  Copyright © 2019 Shane. All rights reserved.
+//
+
+import Foundation
+import WatchKit
+
+class RowController: NSObject{
+    
+    @IBOutlet weak var rowLabel: WKInterfaceLabel!
+}
